0dfb4804ea6e32aa2c7e1c0453667ab00ce6054c
Joana NicolaasPonder <joanaponder@gmail.com>
2024-08-22 09:51:39 +0200

    add ball reset

--- ball.gd
+++ ball.gd
@@ -3,20 +3,26 @@ extends RigidBody2D
 
 var initial_velocity = Vector2(-300, 100)
 var speed = 600.0
+var should_reset = false
+var initial_position = Vector2(640, 320)
 
 func _ready():
 	
-	linear_velocity = initial_velocity
+	_reset_ball()
+	apply_central_impulse(initial_velocity.normalized() * speed)
 
-func _physics_process(delta):
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	state.linear_velocity = linear_velocity.normalized() * speed
 	
-	linear_velocity = linear_velocity.normalized() * speed
+	if should_reset:
+		var next_transform = state.get_transform()
+		next_transform.origin.x = initial_position.x
+		next_transform.origin.y = initial_position.y
+		state.set_transform(next_transform)
+		should_reset = false
+		
 
 func _reset_ball():
 	print('reset ball')
-
-	set_deferred("position", Vector2(640,320))
-	linear_velocity = initial_velocity
-	
-	apply_central_impulse(linear_velocity.normalized() * speed)
+	should_reset = true
 	
